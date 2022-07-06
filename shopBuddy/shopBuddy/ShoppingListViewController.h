855f67bb401f9d9aa5e79b4df1087bbbd95c83db
Rachna Gupta <rachgupta200@gmail.com>
2022-07-06 11:06:03 -0700

    added register and login functionality with segue to empty list view

---
+++ shopBuddy/shopBuddy/ShoppingListViewController.h
@@ -0,0 +1,16 @@
+//
+//  ShoppingListViewController.h
+//  shopBuddy
+//
+//  Created by Rachna Gupta on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShoppingListViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
