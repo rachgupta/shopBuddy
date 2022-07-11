b55544f202ed80614ed877f1d0e18954407c9599
Rachna Gupta <rachgupta200@gmail.com>
2022-07-11 13:51:27 -0700

    added ItemDetail and value file for upcoming objects

---
+++ shopBuddy/shopBuddy/ItemDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  ItemDetailViewController.h
+//  shopBuddy
+//
+//  Created by Rachna Gupta on 7/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ItemDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ shopBuddy/shopBuddy/ItemDetailViewController.m
@@ -0,0 +1,31 @@
+//
+//  ItemDetailViewController.m
+//  shopBuddy
+//
+//  Created by Rachna Gupta on 7/11/22.
+//
+
+#import "ItemDetailViewController.h"
+
+@interface ItemDetailViewController ()
+
+@end
+
+@implementation ItemDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
